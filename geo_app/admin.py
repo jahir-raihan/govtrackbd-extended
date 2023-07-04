0026ee881301276676e5754d2c46654a935157d6
jahir-raihan <raihan.joy10@gmail.com>
2023-07-04 12:34:41 +0600

    Added admin model to admin page with updated listing

--- geo_app/admin.py
+++ geo_app/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['project_code', 'project_name', 'implementing_agency', ]
+    search_fields = ['approval_ref', 'budget', 'capacity', 'category', 'completion_date', 'cumulative_expenditure',
+                     'date_of_approval', 'district', 'division', 'executing_agency', 'expected_life_time',
+                     'expected_till_now', 'note', 'project_name',
+                     'funded_by', 'id', 'image', 'implementing_agency', 'latitude', 'location', 'longitude', 'ministry',
+                     'pd_name', 'physical_progress', 'present_status', 'progress_reporting_date', 'project_code',
+                     'sector', 'short_title', 'start_date', 'status', 'technology_type', 'upazilla']
+
+
+admin.site.register(Project, ProjectAdmin)
