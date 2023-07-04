1119524a66ff26824869210ecf6a0676f091aba7
jahir-raihan <raihan.joy10@gmail.com>
2023-07-04 12:33:49 +0600

    Added url paths

--- geo_app/urls.py
+++ geo_app/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('get-details/', views.get_details, name='get-details'),
+    path('update-ndre-projects-data/', views.update_ndre_projects_data, name='update-ndre-projects-data'),
+    path('update-lged-projects-data/', views.update_lged_projects_data, name='update-lged-projects-data'),
+
 
 ]
