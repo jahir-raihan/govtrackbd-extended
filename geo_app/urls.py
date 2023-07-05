42f3b9d602762516d46b89d342ceb0db76fa066d
jahir-raihan <raihan.joy10@gmail.com>
2023-07-05 18:38:00 +0600

    Added API route

--- geo_app/urls.py
+++ geo_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('get-details/', views.get_details, name='get-details'),
     path('update-ndre-projects-data/', views.update_ndre_projects_data, name='update-ndre-projects-data'),
     path('update-lged-projects-data/', views.update_lged_projects_data, name='update-lged-projects-data'),
+    path('get-all-data/', views.get_data, name='get-all-data')
 
 
 ]
